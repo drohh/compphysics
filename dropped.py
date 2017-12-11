39364e93968fea1014f5f116568ae6dbd222fafd
drohh <danny.rog@live.com>
2017-12-11 17:06:17 -0600

    Added computation for object's landing.

--- dropped.py
+++ dropped.py
@@ -1,7 +1,15 @@
 # This program tracks an object falling from a building of height h, with initial velocity = 0.
+import math
 
+# Calculate the time it takes for the object to hit the ground
 h = float(input("Enter the height (meters) of the building: "))
-t = float(input("Enter the time interval (seconds): "))
 g = 9.81 # acceleration due to gravity
+timeToGround = math.sqrt(h / g * 2)
+print(timeToGround)
+
+# Analysis of object at a particular time
+'''
+t = float(input("Enter the time interval (seconds): "))
 s = g * t**2 / 2 # formula for how far the object falls
 print("The y location of the object is {} meters".format(h-s))
+'''
