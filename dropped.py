71707c10018ffd78d04a17d8e7210016ac5a481b
drohh <danny.rog@live.com>
2017-12-11 16:54:33 -0600

    Added ability to calculate object's y position.

--- dropped.py
+++ dropped.py
@@ -1,1 +1,7 @@
+# This program tracks an object falling from a building of height h, with initial velocity = 0.
 
+h = float(input("Enter the height (meters) of the building: "))
+t = float(input("Enter the time interval (seconds): "))
+g = 9.81 # acceleration due to gravity
+s = g * t**2 / 2 # formula for how far the object falls
+print("The y location of the object is {} meters".format(h-s))
