389b12a8ed98e73cf277c8e8bf2ccafa929780da
drohh <danny.rog@live.com>
2017-12-12 12:05:23 -0600

    Functions for converting coordinates

---
+++ convertcoord.py
@@ -0,0 +1,11 @@
+from math import cos, sin, sqrt, atan, thetarees
+
+def ptoc(r, theta):
+	x = r*cos(theta)
+	y = r*sin(theta)
+	return x,y
+	
+def ctop(x, y):
+	r = sqrt(x**2 + y**2)
+	theta = degrees(atan(y / x))
+	return r,round(theta, 5)
